de8dfdb341263b7d2ff33fc0354b0e1ca32f2acc
Tim Black <tim@alwaysreformed.com>
2020-07-21 14:04:50 -0700

    refactor(calculateTradeBalance): permit tracking an arbitrary number of trades

--- calculateTradeBalance.js
+++ calculateTradeBalance.js
@@ -1,41 +1,42 @@
+// PURPOSE: Display a smart order's current profit or loss
+
 // USAGE:
 // - Install this in Chrome DevTools' Sources > Snippets
-// - Configure the order numbers to track below
-// - Configure the entry values below
+// - Configure the order numbers to track below, and their entry values
 // - Run it on a TradePartner Smart Order page like https://tradepartner.io/trading/smart-orders/71994
 
-window.bullishOrderNumber = 4
-window.bearishOrderNumber = 3
-
-// Negative equals a credit on entry
-window.bullishEntryValue = 0.6
-window.bearishEntryValue = -1
+// Negative entry value equals a credit
+window.ordersToTrack = [
+    { number: 3, entryValue: -1 },
+    { number: 4, entryValue: 0.6 }
+]
 
 window.profitOrLoss = 'loss'
 window.userAcknowledgedNotification = true
 
+// Create scroller element to display output
 const scrollerElement = document.createElement('div')
 scrollerElement.style.cssText = 'overflow: scroll; height: 20rem;'
 document.body.prepend(scrollerElement)
 
+// Get order's current value from the page
 function getOrderCurrentValue (orderNumber) {
-    return Array.from(document.querySelectorAll('h4'))
+    return parseFloat(Array.from(document.querySelectorAll('h4'))
         .filter(el => el.textContent.indexOf(`Order ${orderNumber}`) !== -1)[0]
         .parentNode.parentNode.querySelector('b').textContent
-        .split('$')[1]
+        .split('$')[1])
 }
 
+// Calculate the trade's balance
 function calculateTradeBalance () {
     
-    const bullishCurrentValue = getOrderCurrentValue(window.bullishOrderNumber)
-    const bearishCurrentValue = getOrderCurrentValue(window.bearishOrderNumber)
-
     // Negative equals a net credit on exit, which means a profit
-
-    const bullishTotal = bullishCurrentValue - window.bullishEntryValue
-    const bearishTotal = bearishCurrentValue - window.bearishEntryValue
-
-    const exitTotal = (bullishTotal + bearishTotal) * 100
+    const exitTotal = window.ordersToTrack.map(order => {
+        const currentValue = getOrderCurrentValue(order.number)
+        // Return the current balance for this order
+        return currentValue - order.entryValue
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue) * 100
 
     const oldProfitOrLoss = window.profitOrLoss
 
