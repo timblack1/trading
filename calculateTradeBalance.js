436e496635d00677fde1a2f7935fd141932cf3e1
Tim Black <tim@alwaysreformed.com>
2020-07-23 17:26:02 -0700

    feat(calculateTradeBalance): create browser notifications, use colors

--- calculateTradeBalance.js
+++ calculateTradeBalance.js
@@ -5,12 +5,17 @@
 // - Configure the order numbers to track below, and their entry values
 // - Run it on a TradePartner Smart Order page like https://tradepartner.io/trading/smart-orders/71994
 
+// ----- BEGIN CONFIGURE THESE VALUES -------
+
 // Negative entry value equals a credit
 window.ordersToTrack = [
-    { number: 3, entryValue: -1 },
-    { number: 4, entryValue: 0.6 }
+    { number: 1, entryValue: -1.78 },
+    { number: 2, entryValue: 1.58 }
 ]
 
+// ----- END CONFIGURE THESE VALUES -------
+
+// Set up global variables
 window.profitOrLoss = 'loss'
 window.userAcknowledgedNotification = true
 
@@ -45,19 +50,57 @@ function calculateTradeBalance () {
         : 'loss'
 
     if (oldProfitOrLoss === 'loss' && window.profitOrLoss === 'profit' && window.userAcknowledgedNotification === true) {
+        // Notify the user that the trade has turned profitable
+        const message = 'This trade turned profitable!'
+        // Create window manager-level notification
+        var notification = new Notification(message, {
+          body: `Current profit: $${exitTotal.toFixed(2)}`,
+          // Make the notification replace any previous notifications sent from this same smart order
+          tag: window.location.href
+        });
+        // When the user clicks the notification, focus the tab which sent the notification
+        notification.addEventListener('click', event => {
+          parent.focus();
+          window.focus();
+        })
         // Do not send multiple copies of this notification
         window.userAcknowledgedNotification = false
-        // Notify the user that the trade has turned profitable
-        window.userAcknowledgedNotification = window.confirm('This trade has just turned profitable, so you may want to exit it soon!')
+        // TODO: Is it a problem for TradePartner functionality that this blocks the browser's
+        // JavaScript execution?
+        // window.userAcknowledgedNotification = window.confirm(message)
     }
 
     const outputMessage = `Current ${window.profitOrLoss}: $${exitTotal.toFixed(2)}`
 
     // Prepend output to the scroller
     const div = document.createElement('div')
+    div.style.cssText = `color: ${window.profitOrLoss === 'profit' ? 'green' : 'red'};`
     div.innerHTML = outputMessage
     scrollerElement.prepend(div)
 
 }
 
+// Get the user's permission to display notifications
+function askNotificationPermission() {
+  // Function to actually ask the permissions
+  function handlePermission(permission) {
+    // Whatever the user answers, we make sure Chrome stores the information
+    if(!('permission' in Notification)) {
+      Notification.permission = permission;
+    }
+  }
+
+  Notification.requestPermission()
+  .then((permission) => {
+    handlePermission(permission);
+  })
+}
+
+// Call main methods
+
+// On page load
+askNotificationPermission()
+
+// Every few seconds
 setInterval(calculateTradeBalance, 10000)
+
